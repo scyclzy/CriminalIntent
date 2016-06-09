036ab8fb4eedfa73d2eb875f5e043919c06b05a1
lizhongyuan <261056702@qq.com>
2016-06-09 15:56:54 +0800

    create model , class Crime

--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,25 +1,17 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class CrimeActivity extends ActionBarActivity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.crime, menu);
-        return true;
     }
 
     @Override
