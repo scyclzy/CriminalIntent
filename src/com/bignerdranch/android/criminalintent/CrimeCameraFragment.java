aa8f1c9deef53019b8a68d2d8ff043ec522646a6
lizhongyuan <scyclzy@gmail.com>
2017-03-13 11:58:08 +0800

    SurfaceView, SurfaceHolder, Surface

--- src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -1,12 +1,17 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.io.IOException;
+
 import android.annotation.TargetApi;
 import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +24,7 @@ public class CrimeCameraFragment extends Fragment {
 	private Camera mCamera;
 	private SurfaceView mSurfaceView;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -35,6 +41,53 @@ public class CrimeCameraFragment extends Fragment {
 		});
 		
 		mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceview);
+		SurfaceHolder holder = mSurfaceView.getHolder();
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		
+		holder.addCallback(new SurfaceHolder.Callback() {
+			
+			@Override
+			public void surfaceDestroyed(SurfaceHolder holder) {
+				// we can no longer display on this surface, so stop the preview.
+				if(mCamera != null) {
+					mCamera.stopPreview();
+				}
+			}
+			
+			@Override
+			public void surfaceCreated(SurfaceHolder holder) {
+				// Tell the camera to use this surface as its preview area
+				try {
+					if(mCamera != null) {
+						mCamera.setPreviewDisplay(holder);
+					}
+				} catch(IOException exception) {
+					Log.e(TAG, "Error setting up preview display", exception);
+				}
+			}
+			
+			@Override
+			public void surfaceChanged(SurfaceHolder holder, int format, int width,
+					int height) {
+				
+				if(mCamera == null) {
+					return ;
+				}
+				
+				// The surface has changed size; update the camera preview size
+				Camera.Parameters parameters = mCamera.getParameters();
+				Size s = null; // to be reset in the next section
+				parameters.setPreviewSize(s.width, s.height);
+				mCamera.setParameters(parameters);
+				try {
+					mCamera.startPreview();
+				} catch(Exception e) {
+					Log.e(TAG, "Could not start preview");
+					mCamera.release();
+					mCamera = null;
+				}
+			}
+		});
 		
 		return v;
 	}
