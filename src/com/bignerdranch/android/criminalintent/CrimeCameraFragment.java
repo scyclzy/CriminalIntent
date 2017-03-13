0e03c77877ccb2256b7d5a2925b6c3c570a99a76
lizhongyuan <261056702@qq.com>
2017-03-13 11:00:42 +0800

    create CrimeCameraFragment and CrimeCameraActivity

---
+++ src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CrimeCameraFragment extends Fragment {
+	
+	private static final String TAG = "CrimeCameraFragment";
+	
+	private Camera mCamera;
+	private SurfaceView mSurfaceView;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		
+		View v = inflater.inflate(R.layout.fragment_crime_camera, container, false);
+		
+		Button takePictureButton = (Button)v.findViewById(R.id.crime_camera_takePictureButton);
+		takePictureButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {				
+				getActivity().finish();
+			}
+		});
+		
+		mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceview);
+		
+		return v;
+	}
+	
+}
