0b15da850a9ba58dc87c71ab538b26cea0e78758
lizhongyuan <scyclzy@gmail.com>
2017-03-13 15:40:02 +0800

    start CrimeCameraActivity

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -23,6 +25,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class CrimeFragment extends Fragment {
 	
@@ -33,6 +36,7 @@ public class CrimeFragment extends Fragment {
 	private static final int REQUEST_DATE = 0;
 	
 	private Crime mCrime;
+	private ImageButton mPhotoButton;
 	private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
@@ -70,6 +74,28 @@ public class CrimeFragment extends Fragment {
 			if(NavUtils.getParentActivityName(getActivity()) != null) {
 				getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
 			}
+		}
+		
+		mPhotoButton = (ImageButton)v.findViewById(R.id.crime_imageButton);
+		mPhotoButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Intent i = new Intent(getActivity(), CrimeCameraActivity.class);
+				startActivity(i);
+			}
+		});
+		
+		// if camera is not available, disable camera functionality
+		PackageManager pm = getActivity().getPackageManager();
+		boolean hasACamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA) ||
+				pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT) || 
+				Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD || 
+				Camera.getNumberOfCameras() > 0;
+				
+		if(!hasACamera) {
+			mPhotoButton.setEnabled(false);
 		}
 		
 		mTitleField = (EditText)v.findViewById(R.id.crime_title);
