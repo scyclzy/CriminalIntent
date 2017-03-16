6994df08c5294fd924d814b6038f1609712aafd3
lizhongyuan <scyclzy@gmail.com>
2017-03-16 16:11:52 +0800

    using formated string

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -227,4 +227,28 @@ public class CrimeFragment extends Fragment {
 		super.onStop();
 		PictureUtils.cleanImageView(mImageView);
 	}
+	
+	private String getCrimeReport() {
+		String solvedString = null;
+		if(mCrime.isSolved()) {
+			solvedString = getString(R.string.crime_report_solved);
+		} else {
+			solvedString = getString(R.string.crime_report_unsolved);
+		}
+		
+		String dateFormat = "EEE, MMM dd";
+		String dateString = DateFormat.format(dateFormat, mCrime.getDate()).toString();
+		
+		String suspect = mCrime.getSuspect();
+		if(suspect == null) {
+			suspect = getString(R.string.crime_report_no_suspect);
+		} else {
+			suspect = getString(R.string.crime_report_suspect, suspect);
+		}
+		
+		String report = getString(R.string.crime_report, 
+				mCrime.getTitle(), dateString, solvedString, suspect);
+		
+		return report;
+	}
 }
