fce03a07aadf680a79c2166b9d94c41ce7a207b0
lizhongyuan <scyclzy@gmail.com>
2017-03-16 15:52:28 +0800

    add suspect to model

--- src/com/bignerdranch/android/criminalintent/Crime.java
+++ src/com/bignerdranch/android/criminalintent/Crime.java
@@ -13,12 +13,14 @@ public class Crime {
 	private static final String JSON_SOLVED = "solved";
 	private static final String JSON_DATE = "date";
 	private static final String JSON_PHOTO = "photo";
+	private static final String JSON_SUSPECT = "suspect";
 	
 	private UUID mId;
 	private String mTitle;
 	private Date mDate;
 	private Photo mPhoto;
 	private boolean mSolved;
+	private String mSuspect;
 	
 	public Crime() {
 		// 生成唯一标识符
@@ -36,6 +38,9 @@ public class Crime {
 		if(json.has(JSON_PHOTO)) {
 			mPhoto = new Photo(json.getJSONObject(JSON_PHOTO));
 		}
+		if(json.has(JSON_SUSPECT)) {
+			mSuspect = json.getString(JSON_SUSPECT);
+		}
 	}
 
 	public JSONObject toJSON() throws JSONException {
@@ -48,6 +53,7 @@ public class Crime {
 		if(mPhoto != null) {
 			json.put(JSON_PHOTO, mPhoto.toJSON());
 		}
+		json.put(JSON_SUSPECT, mSuspect);
 		
 		return json;
 	}
@@ -91,5 +97,13 @@ public class Crime {
 
 	public void setPhoto(Photo photo) {
 		mPhoto = photo;
+	}
+
+	public String getSuspect() {
+		return mSuspect;
+	}
+
+	public void setSuspect(String suspect) {
+		mSuspect = suspect;
 	}
 }
